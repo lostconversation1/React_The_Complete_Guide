4c436c38de9d5e68240b9feb7ab5d54c21e79552
gosia <malgorzataflak4@gmail.com>
2020-08-23 15:34:15 +0200

    radium

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
-import person from './Person/Person';
 
 
 class App extends Component {
@@ -55,11 +55,16 @@ class App extends Component {
   }
   render() {
     const style = {
-      backgoundColour: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -86,18 +91,33 @@ class App extends Component {
           name={this.state.persons[2].name}
           age={this.state.persons[2].age}> my hobiess dancing</Person> */}
       </div>
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     };
+    let classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
     return (
-      <div className="App">
-        <h1>hI I AM APP</h1>
-        <p>this working</p>
-        <button
-          style={style}
-          onClick={this.tooglePersonsHandler}>Switch Name</button>
-        {persons}
-      </div>
+      <StyleRoot>
+
+        <div className="App">
+          <h1>hI I AM APP</h1>
+          <p className={classes.join(' ')}>this working</p>
+          <button
+            style={style}
+            onClick={this.tooglePersonsHandler}>Switch Name</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
